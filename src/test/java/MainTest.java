8c365689e594537dfe5b7d2d038874b9eeeca7b3
Marcin Kunert <the.martines@gmail.com>
2020-11-04 12:54:01 +0100

    Przygotowanie testów do zadania

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+
+    private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Test
+    void shouldWorkFrom1To5() {
+        // given
+        provideInput("1", "2", "3", "4", "5", "-2");
+
+        // when
+        Main.main(new String[]{});
+
+        // then
+        String output = outContent.toString().trim();
+        assertThat(output).contains("5, 4, 3, 2, 1");
+        String outputWithoutSpaces = output.replaceAll(" ", "");
+        assertThat(outputWithoutSpaces).contains("1+2+3+4+5=15");
+        assertThat(output).contains("Najmniejsza liczba w liście to 1");
+        assertThat(output).contains("Największa liczba w liście to 5");
+    }
+
+    private void provideInput(String... lines) {
+        String input = String.join("\r\n", lines);
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
+        System.setIn(testIn);
+    }
+
+    @BeforeEach
+    void init() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void cleanup() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+}
